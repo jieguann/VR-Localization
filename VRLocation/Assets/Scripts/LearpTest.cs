d3bdcec72f547cf17c93376a8686a8e4775bb611
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-28 18:10:54 -0400

    learning Lerp

---
+++ VRLocation/Assets/Scripts/LearpTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearpTest : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+
+    public float lerpPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
