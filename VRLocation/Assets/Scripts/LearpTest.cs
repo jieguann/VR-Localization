b5386d6bdd506dd47fa19dfae74f78b404a47116
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-28 18:36:05 -0400

    Lerp

--- VRLocation/Assets/Scripts/LearpTest.cs
+++ VRLocation/Assets/Scripts/LearpTest.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class LearpTest : MonoBehaviour
 {
-    public Transform PointA;
-    public Transform PointB;
+    [SerializeField]public Transform PointA;
+    [SerializeField] public Transform PointB;
+    [SerializeField] public Transform PointC;
+    [SerializeField] public Transform PointX;
 
+    [SerializeField] public Transform pointP;
+    [SerializeField] public Transform pointQ;
+
+    [Range(0.0f,1.0f)]
     public float lerpPosition;
 
+    //private float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lerpPosition = 0;  
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {/*
+        if(lerpPosition <= 1f)
+        {
+            lerpPosition += Time.deltaTime;
+        }
+        else
+        {
+            lerpPosition = 0;
+        }
+        */
+        lerpPosition = lerpPosition <= 1 ? (lerpPosition += Time.deltaTime) : 0;
+
+        pointP.position = Vector3.Lerp(PointA.position, PointB.position, lerpPosition);
+        pointQ.position = Vector3.Lerp(PointB.position, PointC.position, lerpPosition);
         
     }
+
+
 }
