50725cb0853240d07f642220555566363799f04b
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-28 19:39:49 -0400

    update

--- VRLocation/Assets/Scripts/VRLocomotion.cs
+++ VRLocation/Assets/Scripts/VRLocomotion.cs
@@ -1,53 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class VRLocomotion : MonoBehaviour
 {
     public Transform XRRig;
     public Transform head;
 
     public bool canSmoothMove = false;
-
-    private string verticalAxis;
-    private string horizontalAxis;
-    private string triggerButton;
-
     public float moveSpeed = 20;
 
     public bool canSmoothRotate = false;
     public float rotateSpeed = 20;
 
-    public bool canTeleport;
+    public bool canTeleport = false;
     public Transform teleportingHand;
     public LineRenderer line;
     public Vector3 curveHeight;
     public int lineResolution;
+    public Transform reticle;
+
+
+    public float fadeTime;
+    public RawImage fader;
+
+    public Color validStart;
+    public Color validEnd;
+    public Color invalidStart;
+    public Color invalidEnd;
+
+    private string verticalAxis;
+    private string horizontalAxis;
+    private string triggerButton;
+    private bool teleportLock;
+
 
     private void Awake()
     {
         verticalAxis = "XRI_Left_Primary2DAxis_Vertical";
         horizontalAxis = "XRI_Left_Primary2DAxis_Horizontal";
-        triggerButton = "XRI_Left_Trigger";
+        triggerButton = "XRI_Left_TriggerButton";
     }
-    // Start is called before the first frame update
+
+
     void Start()
     {
         line.positionCount = lineResolution;
+        fader.color = Color.clear;
+        teleportLock = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         var verticalValue = Input.GetAxis(verticalAxis);
         var horizontalValue = Input.GetAxis(horizontalAxis);
+
         if (canSmoothMove)
             SmoothMove(verticalValue);
+
         if (canSmoothRotate)
             SmoothRotate(horizontalValue);
+
         if (canTeleport)
             Teleport();
-        
+
     }
 
     void SmoothMove(float axisValue)
@@ -67,34 +84,106 @@ public class VRLocomotion : MonoBehaviour
     {
         Ray ray = new Ray(teleportingHand.position, teleportingHand.forward);
 
-        if(Physics.Raycast(ray, out RaycastHit hit))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
             line.enabled = true;
+            reticle.gameObject.SetActive(true);
+
+            bool validTarget = hit.collider.CompareTag("teleportable");
+
+            if (validTarget)
+            {
+                line.startColor = validStart;
+                line.endColor = validEnd;
+            }
+            else
+            {
+                line.startColor = invalidStart;
+                line.endColor = invalidEnd;
+            }
+
 
             Vector3 startPoint = teleportingHand.position;
             Vector3 endPoint = hit.point;
-            Vector3 midPoint = ((endPoint - startPoint)/2 ) + startPoint;
+            Vector3 midPoint = ((endPoint - startPoint) / 2) + startPoint;
             midPoint += curveHeight;
 
-            for(int i=0; i<lineResolution; i++)
+            // Smooth movement of the curve
+            Vector3 desiredPosition = endPoint - reticle.position;
+            Vector3 smoothVectoDesired = (desiredPosition / 0.2f) * Time.deltaTime;
+            Vector3 reticleEndpoint = reticle.position + smoothVectoDesired;
+
+            reticle.position = reticleEndpoint;
+            reticle.transform.up = hit.normal;
+
+
+            for (int i = 0; i < lineResolution; i++)
             {
                 float t = i / (float)lineResolution;
-                Vector3 startToMid = Vector3.Lerp(startPoint, midPoint, t);
-                Vector3 midToEnd = Vector3.Lerp(midPoint, endPoint, t);
 
-                Vector3 curvePosition = Vector3.Lerp(startToMid,midToEnd,t);
+                Vector3 startToMid = Vector3.Lerp(startPoint, midPoint, t);
+                Vector3 midToEnd = Vector3.Lerp(midPoint, reticleEndpoint, t);
+
+                Vector3 curvePosition = Vector3.Lerp(startToMid, midToEnd, t);
 
                 line.SetPosition(i, curvePosition);
+
             }
 
-            //Straight Line!
-            //line.SetPosition(0,teleportingHand.position);
+            // This is what we used for a straight line!! :D
+            //line.SetPosition(0, teleportingHand.position);
             //line.SetPosition(1, hit.point);
+
+            if (!teleportLock && validTarget && Input.GetButtonDown(triggerButton))
+            {
+                StartCoroutine(FadeTeleport(endPoint));
+            }
+        }
+        else
+        {
+            line.enabled = false;
+            reticle.gameObject.SetActive(false);
+        }
+    }
+
+
+    private IEnumerator FadeTeleport(Vector3 newPosition)
+    {
+        teleportLock = true;
+
+        // Fadein
+
+        float timer = 0;
+
+        while (timer < fadeTime)
+        {
+            fader.color = Color.Lerp(Color.clear, Color.black, timer);
+            yield return new WaitForEndOfFrame();
+
+            timer += Time.deltaTime;
         }
 
-        if (Input.GetButtonDown(triggerButton))
+        fader.color = Color.black;
+
+        //Teleport
+        XRRig.position = newPosition;
+
+        yield return new WaitForSeconds(fadeTime);
+
+
+        //Fadeout
+        timer = 0;
+
+        while (timer < fadeTime)
         {
-            XRRig.position = hit.point;
+            fader.color = Color.Lerp(Color.black, Color.clear, timer);
+            yield return new WaitForEndOfFrame();
+
+            timer += Time.deltaTime;
         }
+
+        fader.color = Color.clear;
+
+        teleportLock = false;
     }
 }
